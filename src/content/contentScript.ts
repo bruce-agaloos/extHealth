6a65cebc888c5389d6a5286a5793b15aad551888
bakasibruceto <abla09027@gmail.com>
2024-06-15 15:51:59 +0800

    Update contentScript.ts

---
+++ src/content/contentScript.ts
@@ -0,0 +1,12 @@
+import { startTimer } from '../utils';
+
+console.log("contentScript loaded");
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     startTimer(5);
+// });
+startTimer(5);
+
+
+
