1d4bdfa19719a4022620cb0cea11ca1bd3fb8cad
bakasibruceto <abla09027@gmail.com>
2024-06-16 03:29:56 +0800

    removed unnecessary codes

--- src/background/backgroundScript.ts
+++ src/background/backgroundScript.ts
@@ -1,13 +1,1 @@
-// Listen for a connection from the DevTools page
-chrome.runtime.onConnect.addListener((port) => {
-  console.assert(port.name === "devtools-page");
-
-  port.onMessage.addListener((message) => {
-    // Handle messages sent from the DevTools page
-    if (message.type === "greeting") {
-      console.log("Received greeting from DevTools page:", message.payload);
-      // Respond back to the DevTools page
-      port.postMessage({ type: "response", payload: "Hello from background script!" });
-    }
-  });
-});
+// Api Stuffs here
