4a0ebb5a3a2301a2ecbc448dd48038f82435d191
bakasibruceto <abla09027@gmail.com>
2024-06-16 03:28:20 +0800

    feature: Toggle Button

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import "./popup.css";
+
+import { Toggle } from "./components";
+
 const Popup = () => {
-  return (
-    <>
-      <h1>Hello</h1>
-      asdsada
-    </>
-  );
+    return (
+        <>
+            <h1>Hello</h1>
+            <Toggle onToggle={(isOn) => {
+                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, { toggleState: isOn });
+                });
+            }} />
+
+        </>
+    );
 };
 
 const container = document.createElement("div");
