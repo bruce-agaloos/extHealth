be941dc90dca3abc78ddbf05720341ccd4bd618e
bakasibruceto <abla09027@gmail.com>
2024-06-16 03:29:34 +0800

    feature: Schedule Injector

--- src/content/contentScript.ts
+++ src/content/contentScript.ts
@@ -1,12 +1,26 @@
 import { startTimer } from '../utils';
+import { stopTimer } from '../utils/schedule';
+
 
 console.log("contentScript loaded");
 
+let isTrue = false;
+// Inside your content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.toggleState !== undefined) {
+        console.log("Toggle state is now:", request.toggleState);
+        // Handle the toggle state change here
+        if (request.toggleState === true) {
+          
+            startTimer(5, true);
+        }
+        if (request.toggleState === false) {
+            console.log("Timer stopped");
+            stopTimer();
+        }
+    }
+});
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     startTimer(5);
-// });
-startTimer(5);
 
 
 
