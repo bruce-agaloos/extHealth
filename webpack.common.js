b0f39d80b854d232f7e05cccc0e7911d1ebfe77d
bakasibruceto <abla09027@gmail.com>
2024-06-15 22:17:43 +0800

    added react configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,48 +1,73 @@
 const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: {
-        popup: path.resolve("./src/popup/popup.html"),
+        popup: path.resolve("./src/popup/popup.tsx"),
         background: path.resolve("./src/background/backgroundScript.ts"),
         content: path.resolve("./src/content/contentScript.ts"),
+      
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].js",
+   
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js','.tsx'], 
     },
     plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
         new CopyPlugin({
             patterns: [
                 { from: 'src/static' },
                 { from: 'src/static/manifest.json', to: 'manifest.json' } // Ensure manifest.json is copied to dist
             ]
         }),
-        new HtmlPlugin({
-            template: path.resolve(__dirname, "src/popup/popup.html"),
-            filename: "popup.html",
-            chunks: ["popup"]
-        }),
+        ...getHtmlPlugins(["popup"]),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.html$/,
-                use: 'html-loader',
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                type: 'asset/resource',
-            },
-        ],
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
     },
-    resolve: {
-        extensions: ['.ts', '.js','.html'], 
+
+    optimization: {
+        splitChunks: {
+            chunks(chunk) {
+                return (
+                    chunk.name !== "content" &&
+                    chunk.name !== "background"
+                );
+            },
+        },
     },
 };
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(
+        (chunk) =>
+            new HtmlPlugin({
+                title: "xHealth",
+                filename: `${chunk}.html`,
+                chunks: [chunk],
+            })
+    );
+}
