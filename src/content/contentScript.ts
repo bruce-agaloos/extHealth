94ee76eb58a75e13166f61edd971ef73866c272e
bakasibruceto <abla09027@gmail.com>
2024-06-24 16:45:52 +0800

    ref: removed unnecessary imports

--- src/content/contentScript.ts
+++ src/content/contentScript.ts
@@ -1,15 +1,16 @@
 import { startTimer } from '../utils';
 import { stopTimer } from '../utils/schedule';
-import { fetchAndScrapeData } from '../utils/api';
 
 
 console.log("contentScript loaded");
-fetchAndScrapeData();
+
+
 let isTrue = false;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.toggleState !== undefined) {
         console.log("Toggle state is now:", request.toggleState);
         if (request.toggleState === true) {
+       
             startTimer(5, true);
         }
         if (request.toggleState === false) {
