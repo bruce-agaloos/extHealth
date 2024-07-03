2995e2385a4de834516a7e6a8896603e6058ddc7
bakasibruceto <abla09027@gmail.com>
2024-07-03 21:38:43 +0800

    refractor: var type

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,21 @@
 export let contentString;
 
 function fetchCondition() {
-  const url = 'http://localhost:5000/get_condition';
+  const url = 'http://localhost:8000/get_condition';
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data); 
       const method = 'lex-rank'; 
       const language = 'english'; 
-      const sentenceCount = 3; 
+      const sentenceCount = '3'; 
       const inputType = 'URL'; 
       const inputText = data.url; 
       summarizeText(method, language, sentenceCount, inputType, inputText)
         .then(summary => {
           data.summary = summary; 
-          console.log('Data with Summary:', data); 
+          contentString = data;
+          console.log('Data with Summary:', contentString); 
         })
         .catch(error => console.error('Error summarizing text:', error));
     })
@@ -24,12 +25,12 @@ function fetchCondition() {
 function summarizeText(
   method: string, 
   language: string, 
-  sentenceCount: number, 
+  sentenceCount: string, 
   inputType: string, 
   inputText: string
 ): Promise<string> {
   return new Promise((resolve, reject) => {
-    const apiUrl = 'http://localhost:5000/summarize';
+    const apiUrl = 'http://localhost:8000/summarize';
     const data = {
       data: [method, language, sentenceCount, inputType, inputText]
     };
@@ -44,7 +45,6 @@ function summarizeText(
       .then(response => response.text()) 
       .then(summary => {
         console.log('Summary:', summary);
-        contentString = summary;
         resolve(summary); 
       })
       .catch(error => {
