6fd1714ab280895d56578935bbcf700e3e28f250
bakasibruceto <abla09027@gmail.com>
2024-07-03 21:39:16 +0800

    refractor: popupHTML  inline styles

--- src/utils/togglePopup.ts
+++ src/utils/togglePopup.ts
@@ -1,16 +1,17 @@
 const popupHTML = `
-<div id="customPopup" style="position: fixed; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: none; justify-content: center; align-items: center; z-index: 1000;">
+<div id="customPopup" style="position: fixed; right: 0; top: 0; width: 300px; background-color: transparent; display: none; justify-content: center; align-items: center; z-index: 1000;">
     <div id="popupContent" style="background-color: white; padding: 20px; border-radius: 5px; color: black;">
-        <h2 id="popupTitle"></h2>
-        <p id="popupDescription"></p>
-        <p id="popupSummary"></p>
-        <button id="closePopupButton" style="color: black;">Close</button>
+        <h2 id="popupTitle" style="font-weight: bold; font-size: 18px; margin-bottom: 10px;" class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3"></h2>
+        <p id="popupSummary" style="font-size: 14px; color: #333;" class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3"></p>
+        <a id="popupUrl" href="" target="_blank" style="color: blue;"></a>
+        <button id="closePopupButton" style="color: black; background-color: #007bff; color: #fff; border: none; border-radius: 5px; padding: 5px 10px; cursor: pointer;"></button>
     </div>
-</div>`;
+</div>
+`;
 
 export function togglePopup(
   show: boolean,
-  content: { title?: string; description?: string; summary?: string } = {}
+  content: { headline?: string; summary?: string; url?: string } = {}
 ): void {
   let popup = document.getElementById("customPopup");
   if (!popup && show) {
@@ -22,18 +23,20 @@ export function togglePopup(
   }
   if (popup) {
     const titleElement = popup.querySelector("#popupTitle");
-    const descriptionElement = popup.querySelector("#popupDescription");
     const summaryElement = popup.querySelector("#popupSummary");
+    const urlElement = popup.querySelector("#popupUrl") as HTMLAnchorElement;
 
-    if (titleElement) titleElement.textContent = content.title || "";
-    if (descriptionElement)
-      descriptionElement.textContent = content.description || "";
+    if (titleElement) titleElement.textContent = content.headline || "";
     if (summaryElement) {
       const paragraphs = content.summary ? content.summary.split(/\n\n/) : [];
       const formattedParagraphs = paragraphs
         .map((paragraph) => `<p style="text-indent: 20px;">${paragraph}</p>`)
         .join("");
       summaryElement.innerHTML = formattedParagraphs;
+    }
+    if (urlElement) {
+      urlElement.href = content.url || "#";
+      urlElement.textContent = content.url ? "More Info" : ""; // Hide or show the link text based on if a URL is provided
     }
 
     popup.style.display = show ? "flex" : "none";
