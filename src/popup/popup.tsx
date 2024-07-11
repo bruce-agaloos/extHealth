d98d2a92bc1d97887a3907d8810f191e984a3cd0
bakasibruceto <abla09027@gmail.com>
2024-07-12 05:41:09 +0800

    feat extension popup UI

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -2,19 +2,75 @@ import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./popup.css";
 
+import { getExtensionState } from "../utils/storage";
+import { sendExtensionStateToContentScript, } from "../utils/general";
 import { Toggle } from "./components";
 
-const Popup = () => {
-    return (
-        <>
-            <h1>Hello</h1>
-            <Toggle onToggle={(isOn) => {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, { toggleState: isOn });
-                });
-            }} />
+const Popup: React.FC<{}> = () => {
+    const [extensionState, setExtensionState] = useState<boolean | null>(false);
+    
+    useEffect(() => {
+        getExtensionState().then((state) => {
+            setExtensionState(state);
+        });
 
-        </>
+    }, []);
+
+    const handleExtensionState = (newState: boolean): void => {
+        setExtensionState(newState);
+        sendExtensionStateToContentScript(newState);
+    };
+    return (
+        <div className="popupContainer">
+            <div id="popupContent" className="popupContent">
+                <div className="board">
+                    <div className="TitleLogo">
+                        <img src="icon.png" alt="Logo" className="logo" />
+                        <h2 id="popupTitle" className="popupTitle">
+                            extHealth <span className="forc">for Chrome</span>
+                        </h2>
+
+                    </div>
+                </div>
+                <div className="containerSummary">
+                    <p id="popupSummary" className="popupSummary">
+                        Health Reminder is <span>enabled</span> for X <span>by default.</span>
+                    </p>
+                    <Toggle
+                        isOn={extensionState}
+                        onChange={handleExtensionState}
+                    />
+                </div>
+                <div className="containerSummary">
+                    <p id="popupSummary" className="popupSummary">
+                        Health Reminder is <span>enabled</span> for Facebook <span className="boldSummary">by default.</span>
+                    </p>
+                    {/* <Toggle
+                        isOn={extensionState}
+                        onChange={handleExtensionState}
+                    /> */}
+                </div>
+                <div className="containerSummary">
+                    <p id="popupSummary" className="popupSummary">
+                        Check Button is <span>enabled</span> for X <span>by default.</span>
+                    </p>
+                    {/* <Toggle
+                        isOn={extensionState}
+                        onChange={handleExtensionState}
+                    /> */}
+                </div>
+                <div className="containerSummary">
+                    <p id="popupSummary" className="popupSummary">
+                        Check Button is <span>enabled</span> for Facebook <span className="boldSummary">by default.</span>
+                    </p>
+                    {/* <Toggle
+                        isOn={extensionState}
+                        onChange={handleExtensionState}
+                    /> */}
+                </div>
+            </div>
+        </div>
+
     );
 };
 
