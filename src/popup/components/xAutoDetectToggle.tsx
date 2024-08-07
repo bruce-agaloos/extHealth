b41fa7a71da5297bb0e42846003353acec3fb945
Rosgen Hizer <63441352+Rokhai@users.noreply.github.com>
2024-08-07 16:28:30 +0800

    add: XAutoDetectToggle

---
+++ src/popup/components/xAutoDetectToggle.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { setXAutoDetectState } from "../../utils/storage";
+import { xAutoDetectProps } from "../../utils/types";
+
+const XAutoDetectToggle: React.FC<xAutoDetectProps> = ({ isOn, onChange }) => {
+    const handleToggleChange = () => {
+        onChange(!isOn);
+        setXAutoDetectState(!isOn);
+    }
+
+    return (
+        <label className="switch">
+            <input
+                type="checkbox"
+                id="toggle-switch"
+                checked={isOn}
+                onChange={handleToggleChange}
+            />
+            <span className="slider round"></span>
+        </label>
+    );
+};
+
+export default XAutoDetectToggle;
