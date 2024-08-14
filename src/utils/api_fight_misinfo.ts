f5f875ecd4df1fb11647734cabbad02e32739ec5
aries joseph tagle <ajtagle12@gmail.com>
2024-08-14 11:29:09 +0800

    added api endpoint export

--- src/utils/api_fight_misinfo.ts
+++ src/utils/api_fight_misinfo.ts
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://127.0.0.1:8000/api/v1/";
+import { API_ENDPOINT } from './endpoint';
 
 /**
  * Sends the tweet content to the detoX API for hate speech detection.
@@ -7,144 +7,19 @@ const API_ENDPOINT = "http://127.0.0.1:8000/api/v1/";
  */
 const factCheckWithGenerateQueries = async (content: string) => {
     // Encode the content to ensure it's safe to include in a URL
-    // const encodedContent = encodeURIComponent(content);
-    // const extraEndpoint = "factCheck"
-    // const response = await fetch(`${API_ENDPOINT}${extraEndpoint}?content=${encodedContent}`);
-    // const data = await response.json();
-    // return data;
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const sampleFact = {
-        result: [
-          {
-            hypothesis: 'sample',
-            premises: [
-              {
-                premise: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio.',
-                relationship: 'contradiction',
-                url: 'https://example.com',
-                title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio.',
-                date: ""
-              },
-              {
-                premise: '2',
-                relationship: 'entailment',
-                url: 'https://example.com',
-                title: 'Some other title',
-                date: ""
-              },
-              {
-                premise: '3',
-                relationship: 'neutral',
-                url: 'https://example.com',
-                title: 'Some 123 title',
-                date: ""
-              },
-              {
-                premise: '4',
-                relationship: 'neutral',
-                url: 'https://example.com',
-                title: 'Some 333 title',
-                date: ""
-              },
-            ],
-          },
-          {
-            hypothesis: 'sample ',
-            premises: [
-              {
-                premise: '1',
-                relationship: 'contradiction',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: "3/4/2021"
-              },
-              {
-                premise: '2',
-                relationship: 'entailment',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: "3/4/2121"
-              },
-              {
-                premise: '3',
-                relationship: 'neutral',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: "33/3/333"
-              },
-            ],
-          },
-          {
-            hypothesis: '100000',
-            premises: [
-              {
-                premise: '1',
-                relationship: 'contradiction',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: ""
-              },
-              {
-                premise: '2',
-                relationship: 'entailment',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: ""
-              },
-              {
-                premise: '3',
-                relationship: 'neutral',
-                url: 'https://example.com',
-                title: 'Some title',
-                date: ""
-              },
-            ],
-          },
-        ],
-      };
-    return sampleFact;
+    const encodedContent = encodeURIComponent(content);
+    const extraEndpoint = "/factCheck"
+    const response = await fetch(`${API_ENDPOINT}${extraEndpoint}?content=${encodedContent}`);
+    const data = await response.json();
+    return data;
 };
 
-const factCheckWithoutGenerateQueries = async (query: string) => {
-    // const encodedContent = encodeURIComponent(string);
-    // const extraEndpoint = "factCheckSingle"
-    // const response = await fetch(`${API_ENDPOINT}${extraEndpoint}?content=${encodedContent}`);
-    // const data = await response.json();
-    // return data;
-
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const samplePremises = [
-        {
-            premise: '1',
-            relationship: 'contradiction',
-            url: 'https://example.com',
-            title: 'Some title',
-            date: ""
-        },
-        {
-            premise: '2',
-            relationship: 'entailment',
-            url: 'https://example.com',
-            title: 'Some title',
-            date: ""
-        },
-        {
-            premise: '3',
-            relationship: 'neutral',
-            url: 'https://example.com',
-            title: 'Some title',
-            date: "3232"
-        },
-        {
-            premise: '4',
-            relationship: 'neutral',
-            url: 'https://example.com',
-            title: 'Some title',
-            date: ""
-        },
-    ];
-
-    return samplePremises;
+const factCheckWithoutGenerateQueries = async (content: string) => {
+    const encodedContent = encodeURIComponent(content);
+    const extraEndpoint = "/factCheckWithoutQueries"
+    const response = await fetch(`${API_ENDPOINT}${extraEndpoint}?content=${encodedContent}`);
+    const data = await response.json();
+    return data;
 };
 
 
