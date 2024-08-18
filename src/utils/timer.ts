35eeaa4cd96dc5acdaa1e60115505a128c1ef51f
bakasibruceto <abla09027@gmail.com>
2024-08-19 05:13:35 +0800

    feature: minutes::seconds

---
+++ src/utils/timer.ts
@@ -0,0 +1,54 @@
+import { getHealthTips } from ".";
+
+let timerActive = true;
+let timerIntervalId: NodeJS.Timeout | null = null;
+
+const startTimer = (
+  minutes: number,
+  seconds: number,
+  repeat: boolean = true
+): NodeJS.Timeout => {
+  const duration = minutes * 60 + seconds;
+  let remainingTime = duration;
+
+  if (timerIntervalId !== null) {
+    clearInterval(timerIntervalId);
+  }
+
+  timerActive = true;
+
+  timerIntervalId = setInterval(() => {
+    remainingTime--;
+    const remainingMinutes = Math.floor(remainingTime / 60);
+    const remainingSeconds = remainingTime % 60;
+    //debug
+    //console.log(`Time remaining: ${remainingMinutes} minutes and ${remainingSeconds} seconds`);
+    
+    if (remainingTime <= 0) {
+      clearInterval(timerIntervalId as NodeJS.Timeout);
+      console.log("Countdown finished");
+
+      getHealthTips()
+        .catch((error) => console.error("Error:", error));
+      if (repeat && timerActive) {
+        startTimer(minutes, seconds, true);
+      } else {
+        timerIntervalId = null;
+      }
+    }
+  }, 1000);
+
+  return timerIntervalId;
+};
+
+const stopTimer = (): void => {
+  if (timerIntervalId !== null) {
+    clearInterval(timerIntervalId);
+    timerIntervalId = null;
+  }
+  timerActive = false;
+  // debug 
+  // console.log("Timer stopped and future timers are prevented.");
+};
+
+export { startTimer, stopTimer };
