507be5148c8c56e7810f2143fdd55d330102ff21
Rosgen Hizer <63441352+Rokhai@users.noreply.github.com>
2024-08-31 22:55:19 +0800

    fix: twitter auto detect theme (white, dim)

--- src/utils/dom-extractor/dom.ts
+++ src/utils/dom-extractor/dom.ts
@@ -7,9 +7,9 @@ import { TweetBodyWrapper, TwitterTheme } from "./types";
  */
 const getXTheme = (): TwitterTheme => {
     const bodyBackgroundColor = getComputedStyle(document.body).backgroundColor;
-    if (bodyBackgroundColor === "rbg(21, 32, 43)") {
+    if (bodyBackgroundColor === "rgb(21, 32, 43)") {
         return TwitterTheme.Dim;
-    } else if (bodyBackgroundColor === "rgb(225, 225, 225)") {
+    } else if (bodyBackgroundColor === "rgb(255, 255, 255)") {
         return TwitterTheme.White;
     } else {
         return TwitterTheme.Dark;
