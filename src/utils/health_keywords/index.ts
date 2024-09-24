9107dab867aa9a393cd63ee597d585871878ec30
Rosgen Hizer <63441352+Rokhai@users.noreply.github.com>
2024-09-24 11:44:32 +0800

    add: mental health keyword

--- src/utils/health_keywords/index.ts
+++ src/utils/health_keywords/index.ts
@@ -2,6 +2,7 @@ import cancers from "./keyword_module/cancer";
 import covid from "./keyword_module/covid";
 import generalHealthKeywords from "./general_health_keywords";
 import polio from "./keyword_module/polio";
+import mental_health from "./mental_health_keyword";
 
 
 
@@ -9,6 +10,7 @@ const allKeywords = [
 ...cancers,
 ...covid,
 ...polio,
+...mental_health,
 ...generalHealthKeywords
 
 
---
+++ src/utils/health_keywords/mental_health_keyword.ts
@@ -0,0 +1,138 @@
+const mental_health = [
+    // 1 Anxiety Disorders
+    "Generalized Anxiety Disorder",
+    "Panic Disorder",
+    "Social Anxiety Disorder",
+    
+    "Specific Phobias",
+    "Agoraphobia",
+    "Claustrophobia",
+    "Acrophobia",
+    "Aerophobia",
+    "Arachnophobia",
+    "Hemophobia",
+    "Trypanophobia",
+    "Cynophobia",
+    "Ophidiophobia",
+    "Mysophobia",
+    "Astraphobia",
+    "Thanatophobia",
+    "Glossophobia",
+    "Nyctophobia",
+    "Coulrophobia",
+    "Dentophobia",
+    "Triskaidekaphobia",
+    "Autophobia",
+    "Erythrophobia",
+    "Xenophobia",
+
+    "Selective Mutism",
+    "Separation Anxiety Disorder",
+    "Substance/Medication-Induced Anxiety Disorder",
+    "Anxiety Disorder Due to Another Medical Condition",
+
+    "Obsessive-Compulsive Disorder",
+    "Obsessive-Compulsive Disorder (OCD)",
+    "Body Dysmorphic Disorder",
+
+    "Obsessive-Compulsive and Related Disorders",
+
+    "Post-Traumatic Stress Disorder",
+
+    // 2 Mood Disorders
+    "Major Depressive Disorder",
+    "Major Depressive Disorder (MDD)",
+
+    "Dysthymia (Persistent Depressive Disorder)",
+    "Persistent Depressive Disorder",
+    "Bipolar Disorder",
+    "Bipolar I Disorder",
+    "Bipolar II Disorder",
+    "Cyclothymic Disorder",
+    "seasonal affective disorder (SAD)",
+    "Premenstrual Dysphoric Disorder",
+
+    // 3 Psychotic Disorders
+    "Schizophrenia",
+    "Schizophreniform Disorder",
+    "Schizoaffective Disorder",
+    "Delusional Disorder",
+    "Brief Psychotic Disorder",
+    "Shared Psychotic Disorder",
+
+    // 4 Eating Disorders
+    "Anorexia Nervosa",
+    "Bulimia Nervosa",
+    "Binge-Eating Disorder",
+    "Avoidant/Restrictive Food Intake Disorder",
+    "Avoidant/Restrictive Food Intake Disorder (ARFID)",
+    "Pica",
+
+    // 5 Dissociative Disorders
+    "Dissociative Identity Disorder",
+    "Multiple Personality Disorder",
+    "Dissociative Amnesia",
+    "Dissociative Fugue",
+    "Depersonalization/Derealization Disorder",
+
+    // 6 Personality Disorders
+    "Antisocial Personality Disorder",
+    "Borderline Personality Disorder",
+    "Histrionic Personality Disorder",
+    "Narcissistic Personality Disorder",
+
+    // 7 Impulse-Control and Conduct Disorders
+    "Intermittent Explosive Disorder",
+    "Kleptomania",
+    "Pyromania",
+    "Conduct Disorder",
+    "Gambling Disorder",
+
+    // 8 Somatic Symptom and Related Disorders
+    "Somatic Symptom Disorder",
+    "Conversion Disorder",
+    "Conversion Disorder (Functional Neurological Symptom Disorder)",
+    "Illness Anxiety Disorder",
+    "Hypochondriasis",
+
+    // 9 Neurodevelopmental Disorders
+    "Attention-Deficit/Hyperactivity Disorder",
+    "Attention Deficit Hyperactivity Disorder",
+    "ADHD",
+    "Autism Spectrum Disorder",
+    "Asperger's Syndrome",
+
+    // 10 Sleep-Wake Disorders
+    "Insomnia Disorder",
+    "Hypersomnolence Disorder",
+    "Narcolepsy",
+    "Obstructive Sleep Apnea Hypopnea Syndrome",
+    "Restless Legs Syndrome",
+    "Sleep-Wake Disorders",
+    "Circadian Rhythm Sleep-Wake Disorders",
+    "Nightmare Disorder",
+    "Night Terrors",
+    "Sleep Apnea",
+    "Insomnia",
+
+    // 11 Sexual Dysfunctions
+    "Erectile Disorder",
+    "Erectile Dysfunction",
+    "Erectile Dysfunction (Impotence)",
+    "Erectile Dysfunction (ED)",
+    
+    "Ejaculation Disorders",
+
+    "Premature Ejaculation",
+    
+
+
+
+
+    
+    
+
+
+];
+
+export default mental_health;
