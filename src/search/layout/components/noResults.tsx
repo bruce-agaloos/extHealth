aab3970f0608921116ec5c96bee6a70ae27218d5
aries joseph tagle <ajtagle12@gmail.com>
2024-09-30 11:18:17 +0800

    fixed alignment of noresults

--- src/search/layout/components/noResults.tsx
+++ src/search/layout/components/noResults.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 
 interface NoResultsProps {
-    message: string;
+  message: string;
 }
 
 const NoResults: React.FC<NoResultsProps> = ({ message }) => {
-    return (
-        <div style={{ color: 'gray', marginBottom: '30px' }}>
-            <span>{message}</span>
-            <img src="sad-emoji.gif" alt=":(" height="30px" width="30px" />
-        </div>
-    );
+  return (
+    <div
+      style={{
+        color: "gray",
+        marginBottom: "30px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <span>{message}</span>
+      <img src="sad-emoji.gif" alt=":(" height="30px" width="30px" />
+    </div>
+  );
 };
 
 export default NoResults;
