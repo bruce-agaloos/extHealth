f1e44735cefd2bf9167bae9e8ad71d198196bd00
bakasibruceto <abla09027@gmail.com>
2024-10-24 09:06:14 +0800

    removed param value

--- src/guide/layout/startPage.tsx
+++ src/guide/layout/startPage.tsx
@@ -17,7 +17,7 @@ export default function StartPage() {
         <div className="groupLink">
           <Link to="/guide.html/pages/1" className="primary-button">Get Started</Link>
           <Link to="/guide.html/finalPage" className="skip-button">Skip Tutorial</Link>
-          <button onClick={() => openPdf("5", "is")}>open pdf</button>
+          <button onClick={() => openPdf("","")}>open pdf</button>
         </div>
       </div>
       <img src="iconBig.png" alt="" />
