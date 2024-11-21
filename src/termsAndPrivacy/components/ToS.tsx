ca03b7fb8b5e20c3b5fa627e68f791562c566ee0
aries joseph tagle <ajtagle12@gmail.com>
2024-11-21 15:49:39 +0800

    modified modal design

--- src/termsAndPrivacy/components/ToS.tsx
+++ src/termsAndPrivacy/components/ToS.tsx
@@ -1,11 +1,10 @@
 // src/TermsOfService.tsx
 import React from "react";
 import { Typography, Container, Box } from "@mui/material";
+import TosContent from "./TosContent";
 
 const TermsOfService = () => {
-  const getPrivacyLink = () => {
-    return chrome.runtime.getURL("documentation.html") + "#/privacy-policy";
-  };
+ 
   return (
     <Container maxWidth="md">
       <Box padding={3}>
@@ -15,121 +14,7 @@ const TermsOfService = () => {
         <Typography variant="subtitle1" color="textSecondary" gutterBottom>
           Last Updated: November 19, 2024
         </Typography>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            AGREEMENT TO TERMS
-          </Typography>
-          <Typography paragraph>
-            Welcome to eXtHealth, a browser extension developed by the students
-            of Don Honorio Ventura State University (DHVSU). eXtHealth is
-            designed to deliver health reminders and fact-checking features to
-            support health-related posts and claims by providing health
-            information from trusted sources. By using eXtHealth, you confirm
-            that you have read, understood, and agree to be bound by these Terms
-            of Service. If you do not agree to these terms, you must uninstall
-            the extension immediately.
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Privacy
-          </Typography>
-          <Typography paragraph>
-            For information on how we handle your data, please refer to our{" "}
-            <a
-              style={{ textDecoration: "underline" }}
-              href={getPrivacyLink()}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Privacy Policy
-            </a>
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Intellectual Property Rights
-          </Typography>
-          <Typography component="div" paragraph>
-            eXtHealth and all its content, including but not limited to source
-            code, design, trademarks, and materials, are the proprietary
-            property of the creators and are protected by intellectual property
-            laws. You are granted a limited, non-commercial license to use the
-            extension for personal use only. Unauthorized reproduction,
-            distribution, or commercial use of the content is prohibited. All
-            rights not explicitly granted in these terms are reserved by us.
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            User Responsibilities
-          </Typography>
-          <Typography component="div" paragraph>
-            You agree to use the extension in a lawful manner, in accordance
-            with all applicable laws and regulations, and to respect the rights
-            of others. You will not engage in any activity that could disrupt or
-            interfere with the proper functioning of the extension, including
-            tampering with its features.
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Disclaimer of Warranties and Limitation of Liability
-          </Typography>
-          <Typography paragraph>
-            eXtHealth is provided "AS IS" without any warranties or guarantees
-            of any kind. The health information offered by eXtHealth is based on
-            available data. While we make every effort to ensure the accuracy
-            and reliability of the information provided, we do not guarantee
-            that the extension will be accurate in every case. By using the
-            extension, you agree that we are not responsible for any issues,
-            inaccuracies, or consequences, including data loss or problems with
-            health-related claims.
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Updates and Modifications
-          </Typography>
-          <Typography paragraph>
-            We reserve the right to update or modify these Terms of Service or
-            to make changes to the eXtHealth extension at any time. Any updates
-            or modifications will become effective immediately upon posting. By
-            continuing to use eXtHealth after such changes, you acknowledge and
-            accept the updated Terms of Service or modifications to the
-            extension.
-          </Typography>
-        </Box>
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Governing Law
-          </Typography>
-          <Typography paragraph>
-            These Terms of Service are governed by the laws of the Philippines.
-            Any disputes arising from these terms shall be resolved in the
-            courts of the Philippines.
-          </Typography>
-        </Box>
-
-        <Box marginTop={3}>
-          <Typography variant="h6" gutterBottom>
-            Contact Information
-          </Typography>
-          <Typography paragraph>
-            For any questions, concerns, or feedback regarding the eXtHealth
-            extension, please feel free to reach out to us using the following
-            contact details:
-          </Typography>
-          <Typography paragraph fontWeight="bold">
-            eXtHealth
-            <br />
-            Don Honorio Ventura State University (DHVSU)
-            <br />
-            Bacolor, Pampanga, Philippines
-            <br />
-            Email: 2021314140@dhvsu.edu.ph
-          </Typography>
-        </Box>
+        <TosContent />
       </Box>
     </Container>
   );
--- src/termsAndPrivacy/forOtherFolders/modal.tsx
+++ src/termsAndPrivacy/forOtherFolders/modal.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Box, Typography, Button, Container } from '@mui/material';
-import TermsOfService from './../components/ToS'; // assuming TermsOfService is in the same directory
-import { setTOS, getTOS } from './../../utils/storage';
+import React, { useState, useEffect } from "react";
+import { Modal, Box, Typography, Button, Container } from "@mui/material";
+import TosContent from "./../components/TosContent"; // assuming TermsOfService is in the same directory
+import { setTOS, getTOS } from "./../../utils/storage";
 
 const TermsOfServiceModal = () => {
   const [open, setOpen] = useState(false);
@@ -10,6 +10,7 @@ const TermsOfServiceModal = () => {
     const checkTOS = async () => {
       // Check if the ToS has been accepted on component mount
       const result = await getTOS();
+      setOpen(true);
       console.log(result);
       if (!result?.tosAccepted) {
         setOpen(true);
@@ -23,41 +24,83 @@ const TermsOfServiceModal = () => {
     setOpen(false); // Close the modal
   };
 
-  const getPrivacyLink = () => {
-    return chrome.runtime.getURL("documentation.html")+'#/privacy-policy';
-  }
-
   return (
-    <Modal open={open} onClose={() => {}}>
+    <Modal
+      open={open}
+      onClose={() => {}}
+      sx={{
+        backdropFilter: "blur(5px)",
+        background: "linear-gradient(180deg, #7075CB, #FFFFFF)",
+      }}
+    >
       <Box
         sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: '80%',
-          maxHeight: '80vh',
-          overflowY: 'auto',
-          bgcolor: 'background.paper',
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: { xs: "80%", sm: "60%", md: "50%" }, // Adjust width for different screen sizes
+          maxHeight: "85vh",
+          overflowY: "auto",
+          bgcolor: "background.paper",
           boxShadow: 24,
           p: 4,
-          borderRadius: 1,
+          // borderRadius: "20px",
         }}
       >
         <Container>
-          <Typography variant="h4" gutterBottom>
+          {/* Centering the Title */}
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{
+              fontFamily: "Lexend Giga",
+              fontWeight: "semibold",
+              textAlign: "center", // Center horizontally
+            }}
+          >
             Terms of Service
           </Typography>
-          <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-            Last Updated: November 3, 2024
-          </Typography>
-          <TermsOfService /> {/* Including the ToS content as a component */}
 
-          <Box display="flex" justifyContent="flex-end" alignItems="center" gap="10px" mt={3}>
-            <a style={{textDecoration: "underline"}} href={getPrivacyLink()} target='_blank' rel="noreferrer noopener">Privacy Policy</a>
+          {/* Centering the Date */}
+          <Typography
+            variant="subtitle1"
+            color="textSecondary"
+            gutterBottom
+            sx={{ textAlign: "center" }} // Center horizontally
+          >
+            Last Updated: November 19, 2024
+          </Typography>
+
+          {/* TosContent with dynamic scroll based on screen size */}
+          <Box
+            sx={{
+              backgroundColor: "rgba(166, 166, 166, 0.09)",
+              height  : { xs: "auto", sm: "auto",},
+              maxHeight: { md: "50vh" }, // Allow more height on small screens
+              overflowY: { md: "auto"}, // Scroll only on larger screens
+              p: 2,
+              borderRadius: "8px", // Optional: Slight rounding for TosContent container
+            }}
+          >
+            <TosContent />
+          </Box>
+
+          {/* Centered Button */}
+          <Box display="flex" justifyContent="center" alignItems="center" mt={3}>
             <Button
               variant="contained"
-              color="primary"
+              sx={{
+                backgroundColor: "#525698",
+                color: "white",
+                borderRadius: "10px", // Slight rounding for button
+                fontWeight: "bold", // Makes text bold
+                fontSize: "1.2rem", // Larger font size
+                padding: "12px 24px", // Increased padding
+                "&:hover": {
+                  backgroundColor: "#40457a",
+                },
+              }}
               onClick={handleAccept}
             >
               I understand and agree
