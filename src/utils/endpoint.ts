c96d6fb2440bcc80b175fc494f44e3fd29f5e5d4
aries joseph tagle <ajtagle12@gmail.com>
2024-12-05 18:57:17 +0800

    Update CSS for links in evidence component and modify API endpoint configuration

--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -1,9 +1,9 @@
-// const API_ENDPOINT = "http://127.0.0.1:8000/api/v1";
+const API_ENDPOINT = "http://127.0.0.1:8000/api/v1";
 
 // REMOVE FOR BOOK BINDING
 
 // CONTENT-UNITY
-const API_ENDPOINT = "https://fact-check-api-production.up.railway.app/api/v1";
+// const API_ENDPOINT = "https://fact-check-api-production.up.railway.app/api/v1";
 
 // MOON-SHINE
 // const API_ENDPOINT = "https://fact-check-api-production-5c7c.up.railway.app/api/v1";
